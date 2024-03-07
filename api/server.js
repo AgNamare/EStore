75d0d1f32e5b80347442085a635f40035056f546
Augustine Namare <agNamare@gmail.com>
2024-03-07 13:53:29 +0300

    Add an errorHandler function and a middleware

--- api/server.js
+++ api/server.js
@@ -41,3 +41,13 @@ app.use((err, req, res, next)=>{
     message
   });
  });
+
+ app.use((err, req, res, next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode, 
+    message
+  });
+ });
